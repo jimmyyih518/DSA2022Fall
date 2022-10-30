97fd025edbdc4acf167d51fceddc1f376aff8610
jimmyyih518 <68363688+jimmyyih518@users.noreply.github.com>
2022-10-30 17:54:50 -0600

    update with week8

---
+++ week8/num_islands.py
@@ -0,0 +1,18 @@
+#Number of islands
+#Islands are connected 1's
+#Water are connected 0's
+#Template code to start
+
+def num_islands(grid):
+	#Observe for the "graph" relationship here
+    #What constitutes a vertex (node), what is an edge?
+    #How to find the neighbors of each vertex?
+    
+    #Define a graph traversal technique
+    
+    #Walk through all grid cells (0s and 1s)
+    #Perform traversal and count number of islands
+    
+    
+    return count
+    
